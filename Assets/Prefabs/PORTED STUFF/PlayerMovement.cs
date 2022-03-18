e11f137349fa19353879bfcfea7050a37aa8c711
Nerys-Thamm <nerysthamm@mgail.com>
2022-03-19 01:20:38 +1300

    SLIDE TO THE LEFT SLIDE TO THE RIGHT

--- Assets/Prefabs/PORTED STUFF/PlayerMovement.cs
+++ Assets/Prefabs/PORTED STUFF/PlayerMovement.cs
@@ -11,6 +11,7 @@ public class PlayerMovement : NetworkBehaviour
 {
     public InputAction moveAction;
     public InputAction jumpAction;
+    public InputAction slideAction;
 
     public CharacterController controller;
     public Transform groundCheck;
@@ -19,16 +20,23 @@ public class PlayerMovement : NetworkBehaviour
 
     public float moveSpeed;
     public float jumpHeight;
-
+    public float initialSlideSpeed;
+    public float maxSlideDuration;
+    Vector3 slideDirection;
+    public float slideTimer;
     public float gravity;
 
     public Vector3 velocity;
     bool isGrounded;
 
+    public Transform cameraTransform;
+
+
     private void Start() {
         controller = GetComponent<CharacterController>();
         moveAction.Enable();
         jumpAction.Enable();
+        slideAction.Enable();
     }
 
     // Update is called once per frame
@@ -46,6 +54,7 @@ public class PlayerMovement : NetworkBehaviour
             // }
 
             isGrounded = false;
+
 
             Collider[] hits = Physics.OverlapSphere(groundCheck.position, groundDistance, groundMask);
 
@@ -70,8 +79,29 @@ public class PlayerMovement : NetworkBehaviour
             float z = moveInput.y;
 
             Vector3 move = Vector3.ClampMagnitude((transform.right * x) + (transform.forward * z), 1.0f);
+            
+            if(slideAction.ReadValue<float>() > 0.0f)
+            {
+                if(slideTimer == 0.0f)
+                {
+                    slideDirection = move;
+                }
+                float velY = velocity.y;
+                velocity = (1.0f - Mathf.Clamp(slideTimer, 0, maxSlideDuration) / maxSlideDuration) * initialSlideSpeed * slideDirection;
+                velocity.y = velY;
+                slideTimer += Time.deltaTime;
+                cameraTransform.position = Vector3.Lerp(cameraTransform.position, transform.position + (transform.up * 0.1f), Time.deltaTime * 10.0f);
+            }
+            else
+            {
+                slideTimer = 0.0f;
+                velocity.x = 0.0f;
+                velocity.z = 0.0f;
+                controller.Move(move * Time.deltaTime * moveSpeed);
+                cameraTransform.position = Vector3.Lerp(cameraTransform.position, transform.position + (transform.up * 1.0f), Time.deltaTime * 10.0f);
+            }
 
-            controller.Move(move * Time.deltaTime * moveSpeed);
+            
 
             if (jumpAction.ReadValue<float>() > 0 && isGrounded)
             {
