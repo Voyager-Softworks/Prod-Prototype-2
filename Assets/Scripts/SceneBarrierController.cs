7352a41d8b2469a1a55814b2a6e48975b01cfdc6
Nerys-Thamm <nerysthamm@mgail.com>
2022-03-22 15:53:08 +1300

    Fixed merge issues

--- Assets/Scripts/SceneBarrierController.cs
+++ Assets/Scripts/SceneBarrierController.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class SceneBarrierController : MonoBehaviour
+public class SceneBarrierController : NetworkBehaviour
 {
+    public bool testSwitch = false;
+    public bool testSwitch2 = false;
+    List<SceneBarrier> barriers = new List<SceneBarrier>();
+
+    public void RegisterBarrier(SceneBarrier barrier)
+    {
+        barriers.Add(barrier);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,36 @@ public class SceneBarrierController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(testSwitch)
+        {
+            testSwitch = false;
+            foreach(SceneBarrier barrier in barriers)
+            {
+                barrier.SwitchBarriers(SceneBarrier.BarrierType.GROUP_A);
+            }
+        }
+        if(testSwitch2)
+        {
+            testSwitch2 = false;
+            foreach(SceneBarrier barrier in barriers)
+            {
+                barrier.SwitchBarriers(SceneBarrier.BarrierType.GROUP_B);
+            }
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    public void CmdSwitchBarriers(SceneBarrier.BarrierType type)
+    {
+        SwitchBarriers(type);
+    }
+
+    [ClientRpc]
+    void SwitchBarriers(SceneBarrier.BarrierType type)
+    {
+        foreach(SceneBarrier barrier in barriers)
+        {
+            barrier.SwitchBarriers(type);
+        }
     }
 }
