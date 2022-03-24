2580a29b830f5bbde9118f678e4b87f9235b82fc
Keane <KeaneCarotenuto@gmail.com>
2022-03-25 00:24:42 +1300

    3D Usernames

---
+++ Assets/WorldNameScript.cs
@@ -0,0 +1,53 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class WorldNameScript : MonoBehaviour
+{
+    public PlayerStats _realStats;
+    public TextMeshProUGUI _worldName;
+    GameObject _clientPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find the client player
+        PlayerStats[] players = GameObject.FindObjectsOfType<PlayerStats>();
+
+        foreach (PlayerStats player in players)
+        {
+            if (player.isLocalPlayer)
+            {
+                _clientPlayer = player.gameObject;
+                break;
+            }
+        }
+
+        if (_realStats == null)
+        {
+            _realStats = GetComponentInParent<PlayerStats>();
+        }
+
+        if (_worldName == null)
+        {
+            _worldName = GetComponent<TextMeshProUGUI>();
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_clientPlayer == null) return;
+
+        if (_realStats != null && _worldName != null)
+        {
+            _worldName.text = _realStats.username;
+        }
+
+        //rotate towards local player
+        transform.LookAt(_clientPlayer.transform);
+    }
+}
